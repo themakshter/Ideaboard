d3caa8898fb3e082178bd6e82399f754e4af44e9
Mohammad Ali Khan <mohammad_ali93@live.com>
2014-11-07 22:37:48 +0000

    Add routes and GET,PUT,POST and DELETE functionality for small board collection. npm install mongodb must be done separately because bson is being a pain to install.

--- development/backend/routes/boards.js
+++ development/backend/routes/boards.js
@@ -1,7 +1,132 @@
-exports.findAll = function(request, response){
-	response.send([{boardName:"testBoard1"},{boardName:"testBoard2"}]);
+var mongo = require("mongodb");
+
+var Server = mongo.Server,
+	db = mongo.Db,
+	BSON = mongo.BSONPure;
+
+var dbName = 'ideaboardDB';
+
+var server = new Server('localhost',27017, {auto_reconnect: true});
+db = new db(dbName,server);
+
+db.open(function(err, db){
+	if(!err){
+		console.log("Connected to '" +dbName+ "' database ");
+		db.collection('boards',{strict:true}, function(err, collection){
+			if(err){
+				console.log("The 'boards' collection doesn't exist. Creating it with sample data...");
+				populateDB();
+			}
+		});
+	}
+});
+
+exports.findById = function(req, res){
+	var id = req.params.id;
+	console.log("Retrieving board : " + id);
+	db.collection('boards',function(err, collection){
+		collection.findOne({'_id':new BSON.ObjectID(id)}, function(err, item){
+			if(err){
+				console.log(err);
+			}else{
+				res.send(item);
+			}
+		});
+	});
 };
 
-exports.findByName = function(request, response){
-	response.send({boardName: request.params.boardName, author:"testBoard1Author", colour:"blue"});
+exports.findAll = function(req, res){
+	db.collection('boards', function(err, collection){
+		collection.find().toArray(function(err,items){
+			res.send(items);
+		});
+	});
 };
+
+exports.addBoard = function(req,res){
+	var board = req.body;
+	console.log('Adding board: ' + JSON.stringify(board));
+	db.collection('boards', function(err, collection){
+		collection.insert(board, {safe:true},function(err,result){
+			if(err){
+				res.send({"error":"An error occured - " + err});
+			}else{
+				console.log("Success : " + JSON.stringify(result[0]));
+				res.send(result[0]);
+			}
+		});
+	});
+};
+
+exports.updateBoard = function(req,res){
+	var id = req.params.id;
+	var board = req.body;
+	console.log("Updating board: " + id);
+	console.log(JSON.stringify(board));
+	db.collection('boards', function(err, collection){
+		collection.update({"_id":new BSON.ObjectID(id)}, board, {safe:true}, function(err, result){
+			if(err){
+				console.log("Error updating board - " +err);
+				res.send({"error": "An error occured - " + err});
+			}else{
+				console.log("" + result + "document(s) updated");
+				res.send(board);
+			}
+		});
+	});
+};
+
+exports.deleteBoard = function(req,res){
+	var id = req.params.id;
+	console.log("Deeleting board : " + id);
+	db.collection('boards', function(err, collection){
+		collection.remove({"_id":new BSON.ObjectID(id)}, {safe:true}, function(err, result){
+			if(err){
+				res.send({'error':'An error occured - ' + err});
+			}else{
+				console.log('' + result + ' document(s) removed');
+				res.send(req.body);
+			}
+		});
+	});
+};
+
+var populateDB = function(){
+	var boards = [
+		{
+			name : "testBoard1",
+			author: "testBoard1Author",
+			colour : "testColour2"
+		},
+		{
+			name : "testBoard2",
+			author: "testBoard2Author",
+			colour : "testColour2"
+		},
+		{
+			name : "testBoard3",
+			author: "testBoard3Author",
+			colour : "testColour3"
+		},
+		{
+			name : "testBoard4",
+			author: "testBoard4Author",
+			colour : "testColour4"
+		},
+		{
+			name : "testBoard5",
+			author: "testBoard5Author",
+			colour : "testColour5"
+		},
+	];
+
+	db.collection('boards', function(err, collection){
+		collection.insert(boards,{safe:true}, function(err, result){});
+	});
+};
+
+
+
+
+
+
--- development/backend/server.js
+++ development/backend/server.js
@@ -7,8 +7,10 @@ var app = express();
 var port = 3000;
 
 app.get('/boards', boards.findAll);
-
-app.get('/boards/:boardName', boards.findByName);
+app.get('/boards/:id', boards.findById);
+app.post('/boards/',boards.addBoard);
+app.put('/boards/:id',boards.updateBoard);
+app.delete('/boards/:id', boards.deleteBoard);
 
 app.listen(3000);
 console.log("Listening on port " + port);
